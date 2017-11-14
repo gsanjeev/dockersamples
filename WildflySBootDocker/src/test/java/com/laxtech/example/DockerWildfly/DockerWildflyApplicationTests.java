8d8ceb4cf22d2bc6e48c24f930e1fc30f94fd939
Sanjeev <gsanjeevtripathi@gmail.com>
2017-11-14 14:19:39 +0530

    wildfly Spring Boot Docker Sample

---
+++ WildflySBootDocker/src/test/java/com/laxtech/example/DockerWildfly/DockerWildflyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.laxtech.example.DockerWildfly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerWildflyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
